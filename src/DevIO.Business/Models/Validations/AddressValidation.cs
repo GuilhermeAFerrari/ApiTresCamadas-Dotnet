11d8f83e32c1d397ec8f2480cf0d9bd18d6a054a
Guilherme Ferrari <guile.ferrari@hotmail.com>
2023-12-10 18:38:02 -0300

    feat: added validations

---
+++ src/DevIO.Business/Models/Validations/AddressValidation.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+
+namespace DevIO.Business.Models.Validations;
+
+public class AddressValidation : AbstractValidator<Address>
+{
+    public AddressValidation()
+    {
+        RuleFor(x => x.Street)
+            .NotEmpty()
+            .WithMessage("The field {PropertyName} must be provided")
+            .Length(2, 200)
+            .WithMessage("The field {PropertyName} must be between {MinLength} and {MaxLength} characters");
+
+        RuleFor(x => x.District)
+            .NotEmpty()
+            .WithMessage("The field {PropertyName} must be provided")
+            .Length(2, 100)
+            .WithMessage("The field {PropertyName} must be between {MinLength} and {MaxLength} characters");
+
+        RuleFor(x => x.ZipCode)
+            .NotEmpty()
+            .WithMessage("The field {PropertyName} must be provided")
+            .Length(8)
+            .WithMessage("The field {PropertyName} must be {MaxLength} characters");
+
+        RuleFor(x => x.City)
+            .NotEmpty()
+            .WithMessage("The field {PropertyName} must be provided")
+            .Length(2, 100)
+            .WithMessage("The field {PropertyName} must be between {MinLength} and {MaxLength} characters");
+
+        RuleFor(x => x.State)
+            .NotEmpty()
+            .WithMessage("The field {PropertyName} must be provided")
+            .Length(2, 50)
+            .WithMessage("The field {PropertyName} must be between {MinLength} and {MaxLength} characters");
+
+        RuleFor(x => x.Number)
+            .NotEmpty()
+            .WithMessage("The field {PropertyName} must be provided")
+            .Length(1, 50)
+            .WithMessage("The field {PropertyName} must be between {MinLength} and {MaxLength} characters");
+    }
+}
---
+++ src/DevIO.Business/Models/Validations/SupplierValidation.cs
@@ -0,0 +1,34 @@
+﻿using DevIO.Business.Utils;
+using FluentValidation;
+
+namespace DevIO.Business.Models.Validations;
+
+public class SupplierValidation : AbstractValidator<Supplier>
+{
+    public SupplierValidation()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("The field {PropertyName} must be provided")
+            .Length(2, 100)
+            .WithMessage("The field {PropertyName} must be between {MinLength} and {MaxLength} characters");
+
+        When(x => x.SupplierType == ESupplierType.PhysicalPerson, () =>
+        {
+            RuleFor(x => x.Document.Length).Equal(CpfValidacao.TamanhoCpf)
+            .WithMessage("The field {PropertyName} must be {ComparisonValue} chacacters and was provided {PropertyValue}");
+
+            RuleFor(x => CpfValidacao.Validar(x.Document)).Equal(true)
+            .WithMessage("The field {PropertyName} is invalid");
+        });
+
+        When(x => x.SupplierType == ESupplierType.LegalPerson, () =>
+        {
+            RuleFor(x => x.Document.Length).Equal(CnpjValidacao.TamanhoCnpj)
+            .WithMessage("The field {PropertyName} must be {ComparisonValue} chacacters and was provided {PropertyValue}");
+
+            RuleFor(x => CnpjValidacao.Validar(x.Document)).Equal(true)
+            .WithMessage("The field {PropertyName} is invalid");
+        });
+    }
+}
