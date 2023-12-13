c6b6c1d947b0ed5c33535b7ae91312222602a22b
Guilherme Ferrari <guile.ferrari@hotmail.com>
2023-12-13 18:57:35 -0300

    feat: added configure to databasecontext

--- src/DevIO.Data/Context/DatabaseContext.cs
+++ src/DevIO.Data/Context/DatabaseContext.cs
@@ -13,4 +13,24 @@ public class DatabaseContext : DbContext
     public DbSet<Supplier> Products { get; set; }
     public DbSet<Address> Adresses { get; set; }
     public DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var property in modelBuilder.Model.GetEntityTypes()
+                                .SelectMany(e => e.GetProperties()
+                                .Where(p => p.ClrType == typeof(string)))
+        {
+            property.SetColumnType("varchar(100)");
+        }
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                                    .SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+        }
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
