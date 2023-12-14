a38cb225ec3889e04edda8c24438451124ae6a97
Guilherme Ferrari <guile.ferrari@hotmail.com>
2023-12-14 20:56:32 -0300

    feat: added view models

---
+++ src/DevIO.Api/ViewModels/AddressViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.ViewModels;
+
+public class AddressViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(200, ErrorMessage = "The {0} field must be between {2} and {1} characters", MinimumLength = 2)]
+    public string? Street { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(50, ErrorMessage = "The {0} field must be between {2} and {1} characters", MinimumLength = 1)]
+    public string? Number { get; set; }
+
+    public string? Complement { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(100, ErrorMessage = "The {0} field must be between {2} and {1} characters", MinimumLength = 2)]
+    public string? District { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(8, ErrorMessage = "The field {0} must be {1} characters", MinimumLength = 8)]
+    public string? ZipCode { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(100, ErrorMessage = "The {0} field must be between {2} and {1} characters", MinimumLength = 2)]
+    public string? City { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(50, ErrorMessage = "The {0} field must be between {2} and {1} characters", MinimumLength = 2)]
+    public string? State { get; set; }
+
+    public Guid Supplier { get; set; }
+}
